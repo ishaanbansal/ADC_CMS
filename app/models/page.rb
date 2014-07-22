0f29fb8f8f0b13d89f30b20e5e8ffae144a6f4fb
ishaanbansal <ishaan.bansal.29@gmail.com>
2014-07-22 21:06:50 +0530

    final config for dropbox

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,9 +1,14 @@
 class Page < ActiveRecord::Base
   #for paperclip
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-                    
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  else
+    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
+                      :storage => :dropbox,
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+                      :path => ":style/:id_:filename"
+  end
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :subject
