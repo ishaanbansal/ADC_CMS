d8d369e88c0bcd46f40b078e84f792184f274602
ishaanbansal <ishaan.bansal.29@gmail.com>
2014-07-22 21:26:04 +0530

    organized dropbox storage

--- app/models/page.rb
+++ app/models/page.rb
@@ -5,7 +5,7 @@ class Page < ActiveRecord::Base
   else
     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
                       :storage => :dropbox,
-                      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                       :path => ":style/:id_:filename"
   end
 
