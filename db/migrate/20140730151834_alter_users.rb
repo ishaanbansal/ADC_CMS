7d80dfe90ae3c7435686aeb5c6c98d18b5f25f51
ishaanbansal <ishaan.bansal.29@gmail.com>
2014-07-31 17:47:03 +0530

    phone changed to big int

--- db/migrate/20140730151834_alter_users.rb
+++ db/migrate/20140730151834_alter_users.rb
@@ -1,6 +1,6 @@
 class AlterUsers < ActiveRecord::Migration
   def up
-    add_column("users", "phone", :integer, :limit => 15, :after => "email")
+    add_column("users", "phone", :bigint, :limit => 15, :after => "email")
     add_column("users", "address", :string, :limit => 150, :after => "phone")
   end
 
